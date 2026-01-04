c04fd380c80d3881d6c691ec85340950e1bec7ba
Ja-sonYun <killa30867@gmail.com>
2026-01-04 17:44:16 +0900

    Chore

--- hosts/aarch64-darwin/alacritty/default.nix
+++
@@ -1,7 +0,0 @@
-{ userhome, username, pkgs, lib, config, ... }:
-{
-  home.file."alacritty.toml" = {
-    target = ".config/alacritty/alacritty.toml";
-    source = toString ./alacritty.toml;
-  };
-}
