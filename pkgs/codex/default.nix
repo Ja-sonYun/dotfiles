11b29fdbe72000ab1ff1205b77cf42c8a02eb4c7
Ja-sonYun <killa30867@gmail.com>
2026-01-07 11:52:34 +0900

    Chore

--- pkgs/codex/default.nix
+++ pkgs/codex/default.nix
@@ -7,7 +7,7 @@ pkgs.lib.npm.mkNpmGlobalPackageDerivation {
   inherit pkgs outputHash;
   name = "openai-codex";
   packages = [
-    "@openai/codex@0.78.0"
+    "@openai/codex@0.79.0"
   ];
   exposedBinaries = [
     "codex"
