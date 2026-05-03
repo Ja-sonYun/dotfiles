0aeec65da39c62db5eb1b992ba2bb0fa915bae30
Ja-sonYun <killa30867@gmail.com>
2026-05-04 01:01:31 +0900

    Chore

--- pkgs/firecrawl-mcp/default.nix
+++ pkgs/firecrawl-mcp/default.nix
@@ -5,7 +5,7 @@ pkgs.lib.mkPackageDerivation {
   hashKey = "firecrawl-mcp";
   packageManager = "npm";
   packageName = "firecrawl-mcp";
-  packageVersion = "3.11.0";
+  packageVersion = "3.14.1";
   name = "firecrawl-mcp";
   exposedBinaries = [
     "firecrawl-mcp"
