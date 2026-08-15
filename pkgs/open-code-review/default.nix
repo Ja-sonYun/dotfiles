3478d0f378732f867c31aa3ff214d2ce51c05253
Ja-sonYun <killa30867@gmail.com>
2026-08-15 14:52:58 +0900

    Chore

--- pkgs/open-code-review/default.nix
+++ pkgs/open-code-review/default.nix
@@ -5,7 +5,7 @@ pkgs.lib.mkPackageDerivation {
   hashKey = "open-code-review";
   packageManager = "npm";
   packageName = "@alibaba-group/open-code-review";
-  packageVersion = "1.9.3";
+  packageVersion = "1.9.4";
   name = "open-code-review";
   exposedBinaries = [ ];
   buildInputs = [
