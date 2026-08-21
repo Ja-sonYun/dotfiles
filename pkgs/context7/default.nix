fdc161ce0ccda73d37c4137a36ecec33f1621645
Ja-sonYun <killa30867@gmail.com>
2026-08-22 02:48:33 +0900

    Chore

--- pkgs/context7/default.nix
+++ pkgs/context7/default.nix
@@ -5,7 +5,7 @@ pkgs.lib.mkPackageDerivation {
   hashKey = "context7-mcp";
   packageManager = "npm";
   packageName = "@upstash/context7-mcp";
-  packageVersion = "4.0.2";
+  packageVersion = "4.0.3";
   name = "context7-mcp";
   exposedBinaries = [
     "context7-mcp"
